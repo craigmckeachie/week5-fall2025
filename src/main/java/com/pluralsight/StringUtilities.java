35a9d725441bbc7865972ad12feafdadb535ee52
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-22 16:34:45 -0400

    StringUtilities capitalize not static

---
+++ src/main/java/com/pluralsight/StringUtilities.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class StringUtilities {
+    public String capitalize(String word) {
+        if (word == null || word.isEmpty()) return word;
+        return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+    }
+}
