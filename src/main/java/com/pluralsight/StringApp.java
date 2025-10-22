3c2a0e47e3ae11308c6436e194e945e9bd85ee42
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-22 16:40:47 -0400

    StringUtilities repeat

--- src/main/java/com/pluralsight/StringApp.java
+++ src/main/java/com/pluralsight/StringApp.java
@@ -5,5 +5,7 @@ public class StringApp {
         StringUtilities utilities = new StringUtilities();
         System.out.println(utilities.capitalize("craig"));
 
+        System.out.println(utilities.repeat("Um",7));
+
     }
 }
--- src/main/java/com/pluralsight/StringUtilities.java
+++ src/main/java/com/pluralsight/StringUtilities.java
@@ -1,8 +1,14 @@
 package com.pluralsight;
 
 public class StringUtilities {
-    public String capitalize(String word) {
+    public static String capitalize(String word) {
         if (word == null || word.isEmpty()) return word;
         return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+    }
+
+    // 5. Repeat a string a given number of times
+    public static String repeat(String text, int times) {
+        if (text == null || times <= 0) return "";
+        return text.repeat(times);
     }
 }
