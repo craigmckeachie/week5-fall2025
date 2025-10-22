3af7fc4bb89dcfbd633f67cb6739a7b3cdeb5b0d
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-22 16:37:22 -0400

    StringUtilities capitalize not static app

---
+++ src/main/java/com/pluralsight/StringApp.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class StringApp {
+    public static void main(String[] args) {
+        StringUtilities utilities = new StringUtilities();
+        System.out.println(utilities.capitalize("craig"));
+
+    }
+}
--- src/main/java/com/pluralsight/ThingyApp.java
+++ src/main/java/com/pluralsight/ThingyApp.java
@@ -1,9 +1,14 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+
 public class ThingyApp {
     public static void main(String[] args) {
         Thingy thingy = new Thingy();
         thingy.foo("My String", 77);
+
+
+       
 
 
 
